7787032af4cbd0844571f1b6231c405d1e03b48f
Petar Yankov <yankov.petar4@gmail.com>
2024-08-30 22:36:59 +0300

    listing records

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,6 +1,33 @@
+import { useSelector } from "react-redux";
 
 function CarList() {
-    return <div>CarList</div>
+    const cars = useSelector((state) => {
+        return state.cars.data;
+    });
+
+    const handleCarDelete = (car) => {
+        //todo
+    }
+
+    const renderedCars = cars.map((car) => {
+        return <div key={car.id} className="panel">
+            <p>
+                {car.name} - ${car.cost}
+            </p>
+            <button
+                className="button is-danger"
+                onClick={() => handleCarDelete(car)}
+            >
+                Delete
+            </button>
+
+        </div>
+    });
+
+    return <div className="car-list">
+        {renderedCars}
+        <hr />
+    </div>
 }
 
 export default CarList;
