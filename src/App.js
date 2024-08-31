1c45236d3a61aba710bcd722b6f933e1bdaf5352
Petar Yankov <yankov.petar4@gmail.com>
2024-08-31 10:07:43 +0300

    stile with css added

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import CarValue from "./components/CarValue";
 
 
 function App() {
-    return <div>
+    return <div className="container is-fluid">
         <CarForm />
-        <CarList />
         <CarSearch />
+        <CarList />
         <CarValue />
     </div>
 }
